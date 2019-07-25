cd270d59c0b4418e6aa0d89114f7de7ea2de163d
lcapps <lcapps5600@gmail.com>
2019-07-25 15:28:35 -0400

    working version

--- assets/logic.js
+++ assets/logic.js
@@ -1,26 +1,16 @@
-trains = []
-console.log("working")
-function chart() {
-    $("#main-table").html("<tr><th>train name</th><th>Destiantion</th><th>Frequency(min)</th><th>Next arrival</th><th>Minutes away</th></tr>")
-    for (var i = 0; i < trains.length; i++) {
-        //make a new row
-        var newRow = $("<tr id=new-row>")
+var firebaseConfig = {
+    apiKey: "AIzaSyAc7E32SwO3fX223ooWvMFomLgMEkhiBP0",
+    authDomain: "first-d5c07.firebaseapp.com",
+    databaseURL: "https://first-d5c07.firebaseio.com",
+    projectId: "first-d5c07",
+    storageBucket: "",
+    messagingSenderId: "625677026881",
+    appId: "1:625677026881:web:d52ba5d05e58f995"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-        // make the new table items for the info & put info in the text for the each new box
-        var newName = $("<td>").text(trains[i].name)
-        var newDestination = $("<td>").text(trains[i].destination)
-        var newFrequency = $("<td>").text(trains[i].frequency)
-        var newTime = $("<td>").text((trains[i].first)+trains[i].frequency)
-        var newArrivalTime = $("<td>").text(trains[i].tMinutesTillTrain)
-
-        //put the name onto the new row
-        newRow.append(newName, newDestination, newFrequency,  newTime, newArrivalTime)
-
-        // put the new row into the table
-        $("#main-table").append(newRow)
-    }
-}
-
+var database = firebase.database()
 $("#submitButton").on("click", function (event) {
     //gather info
     event.preventDefault()
@@ -29,7 +19,10 @@ $("#submitButton").on("click", function (event) {
     var firstTime = $("#first-time").val()
     var trainFrequency = $("#train-frequency").val()
 
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
     // Current Time
     var currentTime = moment();
@@ -48,21 +41,8 @@ $("#submitButton").on("click", function (event) {
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
-    // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
-
-    var train = {
-        name: trainName,
-        destination: trainDestination,
-        first: firstTime,
-        frequency: trainFrequency,
-        tMinutesTillTrain: tMinutesTillTrain
-
-    }
-    console.log(train)
-
-    trains.push(train)
-    console.log(trains)
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
     // clear the textboxes for next input
     $("#add-name").val('')
@@ -70,5 +50,40 @@ $("#submitButton").on("click", function (event) {
     $("#first-time").val('')
     $("#train-frequency").val('')
 
-    chart()
+    var newTrain = {
+        trainName: trainName,
+        trainDestination: trainDestination,
+        firstTime: firstTime,
+        trainFrequency: trainFrequency,
+        arrivalTime: moment(nextTrain).format("hh:mm"),
+        minutesAway: tMinutesTillTrain
+      };
+
+    database.ref().push(newTrain);
+    $("#main-table").empty()
+
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+      
+        // Store everything into a variable.
+        var train_Name = childSnapshot.val().trainName;
+        var train_destination = childSnapshot.val().trainDestination;
+        // var train_time = childSnapshot.val().firstTime;
+        var train_frequency = childSnapshot.val().trainFrequency;
+        var arrival_time = childSnapshot.val().arrivalTime
+        var minutesAway_Away = childSnapshot.val().minutesAway
+        
+        var newRow = $("<tr>").append(
+        $("<td>").text(train_Name),
+        $("<td>").text(train_destination),
+        $("<td>").text(train_frequency),
+        $("<td>").text(arrival_time),
+        $("<td>").text(minutesAway_Away)
+      );
+
+        $("#main-table").append(newRow)
+    })
+
+    
+    
 })
